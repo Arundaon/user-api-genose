2bf458c688c34b398ee5b2bb67557a9aa7519113
Arundaon Ramadhani Yudistira (TIF C) <ary@pop-os.localdomain>
2021-03-11 22:44:15 +0700

    tambah module cors & eksekusi

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 require("dotenv/config");
 const mongoose = require("mongoose");
 const port = process.env.PORT || 8000;
-app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-    );
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "X-Requested-With,content-type"
-    );
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    next();
-});
+app.use(cors());
 app.use(express.json());
 const userRoute = require("./routes/userRoute");
 app.use("/user", userRoute);
